ece06b3b7286e80c54153145776c7863abd1a602
axelersson <90241829+axelersson@users.noreply.github.com>
2024-01-31 12:33:20 +0100

    Nummer 2

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eca5bcd0f09f9b4c2d87c0f997d8ac24b0b80661")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e96798f1e921ec979eadb8d3dc068082884594")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
