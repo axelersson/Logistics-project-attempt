63b3f33e7d00de285fbcb83c06a23187373ef398
axelersson <90241829+axelersson@users.noreply.github.com>
2024-02-15 11:18:13 +0100

    Added url to api

--- client/src/app/services/api.ts
+++ client/src/app/services/api.ts
@@ -39,7 +39,7 @@ export class Client {
     @Optional() @Inject(API_BASE_URL) baseUrl?: string,
   ) {
     this.http = http;
-    this.baseUrl = baseUrl ?? '';
+    this.baseUrl = baseUrl ?? 'http://localhost:5000';
   }
 
   /**
